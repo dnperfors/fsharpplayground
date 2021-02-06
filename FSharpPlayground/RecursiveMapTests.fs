e3f9f8b0997abf02e18f6c720a60fdee40772d9f
David Perfors <dnperfors@gmail.com>
2021-02-06 20:55:34 +0100

    Fix tests so that they are actually running.

--- FSharpPlayground/RecursiveMapTests.fs
+++ FSharpPlayground/RecursiveMapTests.fs
@@ -3,27 +3,27 @@ module FSharpPlayground.RecursiveMapTests
 open Expecto
 open Swensen.Unquote
 
-let map func list =
-    let rec mapImpl func = function
-        | [] -> []
-        | head :: tail -> func head :: mapImpl func tail
-
-    mapImpl func list
-
 let rev list =
     let rec revImpl acc = function
         | [] -> acc
         | head :: tail -> revImpl (head :: acc) tail
     revImpl [] list
-    
+
+let map func list =
+    let rec mapImpl func acc = function
+        | [] -> rev acc
+        | head :: tail -> mapImpl func (func head :: acc) tail
+
+    mapImpl func [] list    
 
 [<Tests>]
-let tests = [
-    testCase "Map" <| fun _ ->
-        test <@ map (fun x -> x * x) [1..5] = [1; 4; 9; 16; 25] @>
-        let result = map id [1..1000000] 
-        test <@ result = [1..1000000] @>
+let tests =
+    testList "Recursive list functions" [
+        testCase "Map" <| fun _ ->
+            test <@ map (fun x -> x * x) [1..5] = [1; 4; 9; 16; 25] @>
+            let result = map id [1..1000000] 
+            test <@ result = [1..1000000] @>
 
-    testCase "Rev" <| fun _ ->
-        test <@ rev [1..5] = [5..1] @>
-]
+        testCase "Rev" <| fun _ ->
+            test <@ rev [1..5] = [5;4;3;2;1] @>
+    ]  
